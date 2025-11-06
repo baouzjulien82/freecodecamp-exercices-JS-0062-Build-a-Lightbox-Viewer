1228a7ab5f59cfcc39550f150dddfc926e89c01a
Julien Baouz <baouzjulien@gmail.com>
2025-11-06 14:29:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// déclaration des variables
+const closeBtn = document.getElementById('close-btn');
+const fullImg = document.getElementById('lightbox-image');
+const lightBox = document.querySelector('.lightbox');
 
+// Event sur le close btn pour fermer lightbox
+closeBtn.addEventListener(('click'), () => {
+  lightBox.style.display = "none";
+});
