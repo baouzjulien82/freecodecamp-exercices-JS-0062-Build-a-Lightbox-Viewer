ec37e9a47136f3175fe39cfe5e5c8e576647f54e
Julien Baouz <baouzjulien@gmail.com>
2025-11-06 14:37:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const closeBtn = document.getElementById('close-btn');
 const fullImg = document.getElementById('lightbox-image');
 const lightBox = document.querySelector('.lightbox');
+const thumbImg = document.querySelectorAll('.gallery-item')
 
 // Event sur le close btn pour fermer lightbox
 closeBtn.addEventListener(('click'), () => {
